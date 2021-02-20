a5a30de100352f9221d3634a1fb17b7626c213a7
Bigharry34 <64849170+Bigharry34@users.noreply.github.com>
2021-02-20 22:09:45 +0000

    Create init.py
    This module is designed for a neat import of the other xrf_package modules into the main.py.

---
+++ xrf_package/init.py
@@ -0,0 +1,16 @@
+# -----------------------------------------------------------------------------
+# extract_headers.py
+
+# -----------------------------------------------------------------------------
+# Final Year Project - XRF Analysis
+# Harry Morgan
+
+# -----------------------------------------------------------------------------
+# PROGRAMME DESCRIPTION
+# This module holds all the XRF package modules for convinient importing.
+
+# -----------------------------------------------------------------------------
+# IMPORT
+# Import the modules and name them accordingly for use in the main.py.
+from xrf_package.read_emsa_file import read_emsa_file
+from xrf_package.extract_headers import extract_headers
