696a54c090315285c07cf1aae1c8ac3b52298d7e
Bigharry34 <64849170+Bigharry34@users.noreply.github.com>
2021-02-20 22:15:00 +0000

    Create extract_headers.py
    This modules has been created in the xrf_package directory. Its purpose is to extract the headers from the data file and arrange it for use in the main.

---
+++ xrf_package/extract_headers.py
@@ -0,0 +1,21 @@
+# -----------------------------------------------------------------------------
+# extract_headers.py
+
+# -----------------------------------------------------------------------------
+# Final Year Project - XRF Analysis
+# Harry Morgan
+
+# -----------------------------------------------------------------------------
+# PROGRAMME DESCRIPTION
+# This module extracts the headers from a list input of type string.
+# These headers are organised into a dictionary and returned to user.
+
+# -----------------------------------------------------------------------------
+# FUNCTIONS
+def extract_headers(list):
+    pass
+
+# -----------------------------------------------------------------------------
+# MAIN
+if __name__ == "__main__":
+    
