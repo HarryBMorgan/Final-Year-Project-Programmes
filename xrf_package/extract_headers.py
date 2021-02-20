a0e29ec4d643fbfc9458550e476431b26540c077
Bigharry34 <64849170+Bigharry34@users.noreply.github.com>
2021-02-20 23:32:41 +0000

    Update exxtract_headers.py
    Basically created a function that searches an input list of type string. The assumption is that the list has elements of the style "#header : information". The header and info are extracted and ready to be put into some storage for use by the user.

--- xrf_package/extract_headers.py
+++ xrf_package/extract_headers.py
@@ -11,11 +11,76 @@
 # These headers are organised into a dictionary and returned to user.
 
 # -----------------------------------------------------------------------------
+# CLASSES
+# CREATE A CLASS TO HOLD HEADER DATA.
+
+# -----------------------------------------------------------------------------
 # FUNCTIONS
 def extract_headers(list):
-    pass
+    
+    # Create header data container.
+    header_data = 
+    
+    # Read every element in list (represents lines from data file).
+    for i in range(len(list)):
+    
+        # Check for "#". This means the element is a header.
+        if list[i][0] == "#":
+        # Run through the element and seperate header and info then format.
+            
+            # Create temp string lists to be extract header & info.
+            header, info = "", ""
+            
+            # Run through element to extract header name.
+            j = 0
+            while list[i][j] != ":":
+                
+                # Append header to header.
+                header += list[i][j]
+                
+                # Incriment j.
+                j += 1
+            
+            # Take info from element.
+            for l in range(j, len(list[i])):
+                
+                # Append info to info.
+                info += list[i][l]
+            
+            # Remove ":" from strings.
+            header_new = header.replace(":", "")
+            info_new = info.replace(":", "")
+            
+            # Delete header & info from memory (reducing variables).
+            del header, info
+            
+            # Trim the whitespace from the header and its info.
+            header = header_new.strip()
+            info = info_new.strip()
+        
+        elif list[i][0] != "#":
+            pass
+    
+    # Pass the data to the user.
+    return header_data
 
 # -----------------------------------------------------------------------------
 # MAIN
 if __name__ == "__main__":
+    # Define test input.
+    list = ["#Poop : Yummy!", "##Anna : 69", "0.135,", "#END of : Test"]
     
+    # Call the function.
+    data_dict = extract_headers(list)
+    
+    # Print to the user.
+    print(data_dict)
+
+# -----------------------------------------------------------------------------
+# METHOD
+# Read the lines in the list. I guess look for the #.
+# If # is there, take everything up to ":" then strip whitespace.
+# Then take rest from after ":".
+# Strip all whitespace.
+# Try converting to a number.
+# If it fails just hmmm...
