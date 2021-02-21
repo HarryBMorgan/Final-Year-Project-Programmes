2de8e444ede055e3354019197c1a7c7e3a9054f0
Bigharry34 <64849170+Bigharry34@users.noreply.github.com>
2021-02-21 16:40:05 +0000

    Update to xrf_package.py and main.py
    This was done to reflect the changes in the extract_info.py. The imports and tests have been adjusted.

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ import xrf_package.xrf_package as xrf
 # Call read_emsa_file.py.
 lines = xrf.read_emsa_file("EDS_1.emsa")
 
-# Call extract_headers.py.
-xrf.extract_headers(lines)
+# Call extract_info.py to get the headers and data lists.
+header_list, spectrum_data = xrf.extract_info(lines)
+
+# Print headers.
+for i in range(len(header_list)):
+     print(header_list[i][0], header_list[i][1])
+
+# Print data from spectrum_data.
+for i in spectrum_data:
+    print(i)
