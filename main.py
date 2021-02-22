1d74155bb59d6d2466355aebc953ec73fba6ef8e
Bigharry34 <64849170+Bigharry34@users.noreply.github.com>
2021-02-22 12:07:42 +0000

    Update main.py
    The main has been updated to create a better graph with some formatting. The y-scale has been set to logarithmic.
    
    Print test replaced with actual visualisation of data.

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
+# -----------------------------------------------------------------------------
+# xrf analysis
+# main.py
+
+# -----------------------------------------------------------------------------
+# Final Year Project - XRF Analysis
+# Harry Morgan
+
+# -----------------------------------------------------------------------------
+# PROGRAMME DESCRIPTION
+# This module acts as the main of the XRF analysis programme.
+# It utalises the xrf_package to take data from a .emsa file type and plots
+# this data on a graph of energy vs intensity.
+
+# -----------------------------------------------------------------------------
+# MAIN
+# Import the modules.
 import numpy as np
+import matplotlib.pyplot as plt
 import xrf_package.xrf_package as xrf
 
-# Call extract_info.py to get the headers and data lists.
+# Call extract_info.py module to get the headers and data lists.
 header_list, spectrum_data = xrf.extract_info(True, "EDS_1.emsa")
 
 # Locate "#XPERCHAN" and "#OFFSET".
 XPERCHAN = xrf.locate_header_info(header_list, "#XPERCHAN")
 OFFSET = xrf.locate_header_info(header_list, "#OFFSET")
 
-# Print results.
-print("Header: ", XPERCHAN[0], "has value : ", XPERCHAN[1])
-print("Header: ", OFFSET[0], "has value : ", OFFSET[1])
+# Create Energy list.
+Energy = []
+for i in range(1, len(spectrum_data) + 1):
+    Energy.append(i * XPERCHAN[-1] + OFFSET[-1])
+
+# Plot the Energy against the Intensit (spectrum_data).
+plt.plot(Energy, spectrum_data)
+plt.xlabel("Energy, eV"); plt.ylabel("Intensity, log-scale")
+plt.xlim(0, 7500); plt.yscale('log')
+plt.title("EDS 1 XRF Data")
+plt.show()
