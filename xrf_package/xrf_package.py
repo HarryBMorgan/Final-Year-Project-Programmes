cea5cc9ea1c9707fffdac498acbfc190cb450335
HarryBMorgan <64849170+HarryBMorgan@users.noreply.github.com>
2021-03-12 17:38:08 +0000

    Changed some module % function names.
    Some modules and functions names have been changed to reflect their new, more generalized structure.

--- xrf_package/xrf_package.py
+++ xrf_package/xrf_package.py
@@ -14,8 +14,8 @@
 # Import the modules and name them accordingly for use in the main.py.
 
 # Import extract_headers function.
-from xrf_package.read_emsa_file import read_emsa_file
-from xrf_package.extract_info import extract_headers, extract_data
+from xrf_package.read_file import read_file
+from xrf_package.extract_info import extract_emsa_headers, extract_data
 from xrf_package.locate_list_element import locate_list_element
 from xrf_package.evaluate_peak import integrate_peak, peak_energy
 from xrf_package.fluorescence_yield import fluorescence_yield
