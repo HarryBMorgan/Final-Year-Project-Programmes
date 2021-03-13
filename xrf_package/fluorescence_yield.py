bf023c0922e0554443c4a9ee75022c4c86c15c4a
HarryBMorgan <64849170+HarryBMorgan@users.noreply.github.com>
2021-03-13 14:47:22 +0000

    Update fluorescence_yield.py
    Added a yield to the dictionary, Pb wm: 0.0292.

--- xrf_package/fluorescence_yield.py
+++ xrf_package/fluorescence_yield.py
@@ -7,18 +7,7 @@
 
 # -----------------------------------------------------------------------------
 # PROGRAMME DESCRIPTION
-# This module...
-
-# -----------------------------------------------------------------------------
-# MODULE VARIABLE
-# These values are the fluorescence yields at each energy level for the
-# elements listed.
-Fluorescence_dict = {"Br wk": 0.618, "Br w1": 3.6e-3, "Br w2": 0.018, \
-                "Br w3": 0.020, "Pb wk": 0.967, "Pb w1": 0.112, \
-                "Pb w2": 0.373, "Pb w3": 0.360, "Cl wk": 0.097, \
-                "Cl w1": 1.2e-4, "Cl w2": 2.4e-4, "Cl w3": 2.4e-4, \
-                "Cs wk": 0.897, "Cs w1": 0.049, "Cs w2": 0.090, \
-                "Cs w3": 0.091}
+# This module locates the correct fluorescence yield and returns it to user.
 
 # -----------------------------------------------------------------------------
 # FUNCTIONS
@@ -27,16 +16,13 @@ def fluorescence_yield(Element, Yield):
 # "w1") as type str and returns the fluorescence yield for that element at
 # that shell.
 
-    # The list of fluorescence yields is defined here. The Dictionary acts as
-    # a way to convert the Yield str input into a position int of the related
-    # fluorescence yield in the list.
     Fluorescence_dict = {"Br wk": 0.618, "Br w1": 3.6e-3, "Br w2": 0.018, \
                     "Br w3": 0.020, "Pb wk": 0.967, "Pb w1": 0.112, \
-                    "Pb w2": 0.373, "Pb w3": 0.360, "Cl wk": 0.097, \
-                    "Cl w1": 1.2e-4, "Cl w2": 2.4e-4, "Cl w3": 2.4e-4, \
-                    "Cs wk": 0.897, "Cs w1": 0.049, "Cs w2": 0.090, \
-                    "Cs w3": 0.091}
-
+                    "Pb w2": 0.373, "Pb w3": 0.360, "Pb wm": 0.0292, \
+                    "Cl wk": 0.097, "Cl w1": 1.2e-4, "Cl w2": 2.4e-4, \
+                    "Cl w3": 2.4e-4, "Cs wk": 0.897, "Cs w1": 0.049, \
+                    "Cs w2": 0.090, "Cs w3": 0.091}
+    
     # Find which yield we're talking about and return it to the user.
     return Fluorescence_dict[Element + " " + Yield]
 
