46716645f9b2123d43554c127a67272614bd571a
HarryBMorgan <64849170+HarryBMorgan@users.noreply.github.com>
2021-04-01 21:03:55 +0100

    Update main.py
    There is now a case for all 4 data files I have. Gray out the ones you don't want to use.
    
    There has been a change in the # Extraction # section. There is now a check for the type of file being used for data along with file specific extraction procedures.

--- main.py
+++ main.py
@@ -17,6 +17,7 @@
 # Import the modules.
 import matplotlib.pyplot as plt
 from math import sqrt
+from numpy import linspace
 import xrf_package.xrf_package as xrf
 
 
@@ -24,11 +25,35 @@ import xrf_package.xrf_package as xrf
 # Contained here are the file names and gates within which the peaks reside.
 # The peak gate limits are in units of keV.
 
-# Information for the EDS_1 data.
-File_name = "EDS_1.emsa"
-X_lim = [[1.42, 1.62], [2.26, 2.55], [2.55, 2.78], [4.19, 4.43]]
-Fluorescence_shell = ["wl", "wm", "wk", "wl"]
-Axis_upper_lim = 10
+# EDS_1.emsa
+# File_name = "EDS_1.emsa"
+# X_lim = [[1.42, 1.62], [2.26, 2.55], [2.55, 2.78], [4.19, 4.43]]
+# Fluorescence_shell = ["wl", "wm", "wk", "wl"]
+# Y_scale = "log"
+# Axis_upper_lim = 10
+
+# S1-20kV-08nA-x100_pt1.psmsa
+# File_name = "S1-20kV-08nA-x100_pt1.psmsa"
+# X_lim = [[1.41, 1.69], [2.26, 2.58]]
+# Fluorescence_shell = ["wl", "wm"]
+# Y_scale = "log"
+# Axis_upper_lim = 20
+
+# FAPbBr_40keV_powdered.Spe
+# File_name = "FAPbBr_40keV_powdered.Spe"
+# Beam_eV = 40 # keV
+# # X_lim = [[], []]
+# Fluorescence_shell = ["wl", "wm"]
+# Y_scale = "linear"
+# Axis_upper_lim = 20
+
+# FAPbBr_40keV_crystal.Spe
+File_name = "FAPbBr_40keV_crystal.Spe"
+Beam_eV = 40 # keV
+# X_lim = [[], []]
+Fluorescence_shell = ["wl", "wm"]
+Y_scale = "linear"
+Axis_upper_lim = 20
 
 # Common variables.
 Element = ["Br", "Pb", "Cl", "Cs"]
@@ -38,47 +63,59 @@ Atten_files = ["Br_attenuation.txt", "Pb_attenuation.txt",
 
             # ---           EXTRACTION         --- #
 # This section takes the headers and data from the spectrum file. It is then
-# sorted and formatted.
+# sorted and formatted. There is a check for which type the file is as there
+# are different procedures to follow for each.
 
-# Call read_emsa_file.py module to get the information from the .emsa file.
+# Call read_file.py module to get the information from the file.
 File_data = xrf.read_file(File_name)
 
-# Call extract_info.py to get header information and data.
-Header_list, Data_loc = xrf.extract_emsa_headers(File_data)
-Energy, Spectrum_data = xrf.extract_data(File_data, Data_loc)
+# The extraction for msa type files.
+if "msa" in File_name:
+    # Call extract_info.py to get header information and data.
+    Header_list, Data_loc = xrf.extract_msa_headers(File_data)
+    Energy, Spectrum_data = xrf.extract_data(File_data, Data_loc)
 
-# Locate "#XPERCHAN", "#OFFSET" and "XUNITS". Adjust values based on XUNITS.
-XPERCHAN = xrf.locate_list_element(Header_list, "#XPERCHAN")
-OFFSET = xrf.locate_list_element(Header_list, "#OFFSET")
-XUNITS = xrf.locate_list_element(Header_list, "#XUNITS")
+    # Locate "#XPERCHAN", "#OFFSET" and "XUNITS". Adjust values based on XUNITS.
+    XPERCHAN = xrf.locate_list_element(Header_list, "#XPERCHAN")
+    OFFSET = xrf.locate_list_element(Header_list, "#OFFSET")
+    XUNITS = xrf.locate_list_element(Header_list, "#XUNITS")
 
-# All Energy outputs will be altered to be in units of keV.
-if XUNITS == "eV":
-    XPERCHAN *= 1e-3
-    OFFSET *= 1e-3
+    # All Energy outputs will be altered to be in units of keV.
+    if XUNITS == "eV":
+        XPERCHAN *= 1e-3
+        OFFSET *= 1e-3
 
-elif XUNITS == "MeV":
-    XPERCHAN *= 1e3
-    OFFSET *= 1e3
+    elif XUNITS == "MeV":
+        XPERCHAN *= 1e3
+        OFFSET *= 1e3
+
+    # Create Energy list if the output from extract_data are equal. (In this case
+    # there is no energy column and one is required.)
+    if Energy == Spectrum_data:
+        Energy = []
+        
+        for i in range(1, len(Spectrum_data) + 1):
+            Energy.append(i * XPERCHAN + OFFSET)
+
+# The extraction for Spe type files.
+elif "Spe" in File_name:
+    # Call extract_data to get data.
+    Eh, Spectrum_data = xrf.extract_data(File_data, 12)
+
+    # Create Energy list.
+    Energy = linspace(0, Beam_eV, len(Spectrum_data))
 
 
             # ---           VISUALISATION          --- #
-# Create Energy list if the output from extract_data are equal. (In this case
-# there is no energy column and one is required.)
-if Energy == Spectrum_data:
-    Energy = []
-    
-    for i in range(1, len(Spectrum_data) + 1):
-        Energy.append(i * XPERCHAN + OFFSET)
-
 # Plot the Energy against the Intensities (Spectrum_data).
 plt.plot(Energy, Spectrum_data)
 plt.xlabel("Energy, keV", fontsize = 20)
-plt.ylabel("Intensity, log-scale", fontsize = 20)
+plt.ylabel("Intensity (" + Y_scale + ")", fontsize = 20)
 plt.xlim(0, Axis_upper_lim)
-plt.yscale('log')
+plt.ylim(0, max(Spectrum_data) + max(Spectrum_data) / 10)
+plt.yscale(Y_scale)
 plt.title(File_name + ": XRF Data", fontsize = 20)
-#plt.show()
+plt.show()
 
 
             # ---           ANALYSIS           --- #
@@ -99,12 +136,18 @@ for i, val in enumerate(X_lim):
 # Get attenuation data.
 Attenuation = []
 for i, val in enumerate(Atten_files):
-    Attenuation.append(xrf.attenuation(val, Peak_energy[i]))
+    try:
+        Attenuation.append(xrf.attenuation(val, Peak_energy[i]))
+    except IndexError:
+        break
 
 # True number of atoms.
 Flu_yield = []
 for i, val in enumerate(Element):
-    Flu_yield.append(xrf.fluorescence_yield(val, Fluorescence_shell[i]))
+    try:
+        Flu_yield.append(xrf.fluorescence_yield(val, Fluorescence_shell[i]))
+    except IndexError:
+        break
 
 Atom_num, Atom_num_err = [], []
 for i, val in enumerate(Count):
@@ -113,9 +156,12 @@ for i, val in enumerate(Count):
     # Atom_num.append(val / (Flu_yield[i] * Attenuation[i]))
 
 # Ratio of Br to Cl.
-X_Br = '%.2f' %(Atom_num[0] / (Atom_num[0] + Atom_num[2]))
-X_Cl = '%.2f' %(Atom_num[2] / (Atom_num[0] + Atom_num[2]))
-X = [X_Br, "    ", X_Cl, "    "]
+try:
+    X_Br = '%.2f' %(Atom_num[0] / (Atom_num[0] + Atom_num[2]))
+    X_Cl = '%.2f' %(Atom_num[2] / (Atom_num[0] + Atom_num[2]))
+    X = [X_Br, "    ", X_Cl, "    "]
+except IndexError:
+    X = ["    ", "    ", "    ", "    "]
 
 # Total number of atoms is calculated.
 Atom_tot = sum(Atom_num)
@@ -137,12 +183,15 @@ print("  :" + \
     "  Halide Ratio  :")
 
 for i, val in enumerate(Element):
-    print('%.2s' %val + \
-        '%.13s' %(":  " + str('%.2f' %Peak_energy[i]) + " keV            ") + \
-        '%.19s' %(": " + str('%.2e' %Count[i]) + " +/- " + \
-            str('%.0f' %Count_err[i]) + "                                ") + \
-        '%.21s' %(":       " + str('%.4f' %Flu_yield[i]) + "             ") + \
-        '%.16s' %(":   " + str('%.2e' %Attenuation[i]) + "               ") + \
-        '%.16s' %(":    " + str('%.2f' %Percent[i]) + " %                ") + \
-        '%.17s' %(":      " + str(X[i]) + "                              ") + \
-        '%.1s' %":")
+    try:
+        print('%.2s' %val + \
+            '%.13s' %(":  " + str('%.2f' %Peak_energy[i]) + " keV            ") + \
+            '%.19s' %(": " + str('%.2e' %Count[i]) + " +/- " + \
+                str('%.0f' %Count_err[i]) + "                                ") + \
+            '%.21s' %(":       " + str('%.4f' %Flu_yield[i]) + "             ") + \
+            '%.16s' %(":   " + str('%.2e' %Attenuation[i]) + "               ") + \
+            '%.16s' %(":    " + str('%.2f' %Percent[i]) + " %                ") + \
+            '%.17s' %(":      " + str(X[i]) + "                              ") + \
+            '%.1s' %":")
+    except IndexError:
+        break
